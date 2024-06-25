7440c8c76d2f5cf0de80d57fc8e2280810512a66
Logan Gore <lgore93@gmail.com>
2024-06-25 08:41:11 -0700

    Initial structure

---
+++ day15/src/main.rs
@@ -0,0 +1,28 @@
+fn main() {
+    let input = include_str!("../data/input.txt");
+    println!("Problem 1: {}", problem1(input));
+    println!("Problem 2: {}", problem2(input));
+}
+
+fn problem1(input: &str) -> isize {
+    0
+}
+
+fn problem2(input: &str) -> isize {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_problem1() {
+        let input = include_str!("../data/sample.txt");
+    }
+
+    #[test]
+    fn test_problem2() {
+        let input = include_str!("../data/sample.txt");
+    }
+}
